ede00fe94497857cfeda491fb142181ed6e4c910
danielez83 <daniele.zannoni@gmail.com>
2019-09-05 13:30:09 +0200

    Update display_cumulative_distribution.R
    Solved bug: wrong declaration of indexes.

--- display_cumulative_distribution.R
+++ display_cumulative_distribution.R
@@ -2,17 +2,7 @@
 # Author: Daniele Z.
 # Date: 03/09/2019
 
-# SETTINGS --------------------------------------------------------
-start_row_index <- 3 # first line of interest
-last_row_index <- length(abakus_data$Index) # last line of interest
-start_column_index <- 3 # first channel of interest
-last_column_index <- length((abakus_data)) #last channel of interest
-
-# Define size classes
-size_classes <- c(0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.9,3.2,3.5,3.9,4.3,4.8,5.3,5.8,6.4,7.1,7.8,8.6,9.5,10.5,11.6,12.8,14.1,15.5,80.0)
-# -----------------------------------------------------------------
-
-# Load ABAKUS data
+# Load ABAKUS data -----------------------------------------------
 # file_name <- "test_data.txt" # Select ABACUS file name manually
 # Select file using Windows Prompt
 file_name <- choose.files(default = "", caption = "Select file",
@@ -24,7 +14,17 @@ abakus_data <- read.delim2(file = file_name,
                            skip = 6,
                            header = TRUE)
 rm(file_name) # remove variables
+# -----------------------------------------------------------------
 
+# SETTINGS --------------------------------------------------------
+start_row_index <- 3 # first line of interest
+last_row_index <- length(abakus_data$Index) # last line of interest
+start_column_index <- 3 # first channel of interest
+last_column_index <- length((abakus_data)) #last channel of interest
+
+# Define size classes
+size_classes <- c(0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.9,3.2,3.5,3.9,4.3,4.8,5.3,5.8,6.4,7.1,7.8,8.6,9.5,10.5,11.6,12.8,14.1,15.5,80.0)
+# -----------------------------------------------------------------
 
 
 # Calculate cumulative distribution
