b9bdf957af3c25f331e8c7da64943c0e88960121
AzzurraS <45337743+AzzurraS@users.noreply.github.com>
2019-10-02 17:40:14 +0200

    Add files via upload

--- optimized normalization.R
+++ optimized normalization.R
@@ -118,7 +118,7 @@ matplot(df_STD[,1],df_STD[,2:4],type='l', col = rainbow(3), log = "xy",
         xlab = expression(paste("Diameter (", mu, "m)")),
         lwd=1, lty=1) 
 legend("topright",colnames(df_STD[,2:4]),col=rainbow(3),cex=0.6,fill=rainbow(3))
------------------------------------------------------------------------------------------
+
 #SECOND UPLOAD_new set of particle size (FD069)  
   
 rm(filelist, normalization,volume)
@@ -192,12 +192,20 @@ fit <- lm(y ~ x, data = R2_df)
 abline(fit, col="darkturquoise", lwd=2, lty=3)
 legend("right", bty="n", legend=paste("adj R2 is", format(summary(fit)$adj.r.squared, digits=4)))
 summary_fit<-summary(fit)
-
 #fit third degree polynomial equation:
 fit3 <- lm(y~poly(x,3,raw=TRUE))
 #generate range of 50 numbers starting from 0 and ending at 210
 xx <- seq(0,210, length=50)
 lines(xx, predict(fit3, data.frame(x=xx)), col="blue") #ADD BEST LINE FIT 
+
+#TESTs PLOT (WITHOUT milliQ)
+matplot(col.sums[,1],col.sums[,2:4], type="l",col = rainbow(4), log="xy",
+        xlab = expression(paste("Diameter (", mu, "m)")),
+        ylab= "Test (pt/ml)",
+        main="Tests (FD069) with new set of diameters (3 um<d<96 um)",
+        lwd=1, lty=1)
+axis(1, at=seq(3, 47, by=3), labels = FALSE)
+legend("topright",colnames(col.sums[,2:4]),col=rainbow(4),cex=0.6,fill=rainbow(4))
 
 matrix<-data.frame(t(col.sums[,2:4])) 
 colnames(matrix) <- new_size_classes
@@ -231,37 +239,139 @@ df<-data.frame(x1,y1)
 plot(y1~x1,pch=20, col="deeppink3", 
       ylab = "R2",
       xlab = expression(paste("Diameter (", mu, "m)")),
- title("Scatterplot R2 per channel", outer=T, line=1, cex.main=1.5))
- axis(3, at=seq(3, 45, by=3), labels = T)
+ main="Scatterplot R2 per channel" )
+ axis(1, at=seq(3, 50, by=3), labels = T)
 
 #Put down the remanent outliers in order to fit the best polyf. to the scatterplot 
  R_square<-data.frame(R_square)
  for(i in 1:14){
-   if((R_square[i,]==0.0000000))
+   if((R_square[i,]==0.0000000))   #valutare se usare una soglia meno stringente
      R_square[i+1,]<- 0.0000000
  } 
+ 
+ #NEW SCATTERPLOT WITH THE BEST POLYLINE FITTED BY HAND
+ y<-R_square$R_square
+ x<-new_size_classes[1:15]
+ scatterplot_R2_diameters<-data.frame(x,y)
+ plot(y~x,pch=20, col="deeppink3", 
+      ylab = "R2",
+      xlab = expression(paste("Diameter (", mu, "m)")),
+      main="Scatterplot R2 per channel" )
+ axis(1, at=seq(3, 50, by=3), labels = T)
 
+ #fit third degree polynomial equation:
+ fit3 <- lm(y~poly(x,3,raw=TRUE))
+ #generate range of 45 numbers starting from 0 and ending at 200
+ xy <- seq(0,200, length=45)
+ lines(xy, predict(fit3, data.frame(x=xy)), col="blue") #ADD BEST LINE FIT 
+ #fit eighth degree polynomial equation:
+ fit8 <- lm(y~poly(x,8,raw=TRUE))
+ #generate range of 45 numbers starting from 0 and ending at 200
+ xx <- seq(0,200, length=45)
+ lines(xx, predict(fit8, data.frame(x=xx)), col="deeppink3") #ADD BEST LINE FIT 
+
+#0.9105 R2 fit3, 0.987 R2 fit8
+ 
 #GGPLOT WITH POLYLN and equation 
 library("ggpmisc", lib.loc="~/R/win-library/3.5")
+ y<-R_square$R_square
+ x<-new_size_classes[1:15]
+scatterplot_R2_diameters<-data.frame(x1,y) 
+ 
 colnames(scatterplot_R2_diameters)<-c("Diameters (um)","R2")
-formula <- y ~ poly(x, 8, raw = TRUE)
+formula <- y ~ poly(x, 3, raw = TRUE)
 ggplot(scatterplot_R2_diameters, aes(x, y)) +  
   labs(x = "Diameter (um)")+
   labs(y = "R2")+ coord_cartesian(ylim = (0:1))+
   geom_point() +
-  labs(title = "R2 per channel")+ theme(plot.title = element_text(hjust = 0.5))+
+  labs(title = "Correction")+ theme(plot.title = element_text(hjust = 0.5))+
   geom_smooth(method = "lm", formula = formula) +
-  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(adj.rr.label), sep = "~~~~")),
+  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(rr.label), sep = "~~~~")),
                formula = formula, rr.digits = 3, coef.digits = 2, parse = TRUE)
 
-#TESTs PLOT (WITHOUT milliQ)
-matplot(col.sums[,1],col.sums[,2:4],type='l', col = rainbow(4), log="xy",
-        xlab = expression(paste("Diameter (", mu, "m)")),
-        ylab= "Test (pt/ml)",
-        lwd=1, lty=1)
-axis(1, at=seq(3, 47, by=3), labels = FALSE)
-legend("topright",colnames(col.sums[,2:4]),col=rainbow(4),cex=0.6,fill=rainbow(4))
 
+#New df with tests normalized for the graphs' underneathed area 
+col.sums_subset <- data.frame(col.sums[,2:4])
+col.sums_subset <-apply(data.frame(col.sums_subset),2,sum)
+normalized_tests <-col.sums[,2:4]/col.sums_subset
+
+
+# PLOT TESTS (20K, 3.5k, 7k pt/ml) NORMALIZED for the area
+new_diam<-c(3:33)
+normalized_tests<-data.frame(new_diam, normalized_tests)
+
+x<-normalized_tests[,1]
+y<-normalized_tests[,2]
+formula1 <- y ~ poly(x, 3, raw = TRUE)
+plot1<-ggplot(normalized_tests, aes(x, y)) +  
+  labs(x = "Diameter (um)")+
+  labs(y = "STD_FD069 (pt/ml)")+
+  geom_point() +
+  labs(title = "FD069 20k pt/ml normalized for the area")+ theme(plot.title = element_text(hjust = 0.5))+
+  geom_smooth(method = "lm", formula = formula1) +
+  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(rr.label), sep = "~~~~")),
+               formula = formula1, rr.digits = 3, coef.digits = 2, parse = TRUE)
+
+x<-normalized_tests[,1]
+y1<-normalized_tests[,3]
+formula2 <- y1 ~ poly(x, 3, raw = TRUE)
+plot2<-ggplot(normalized_tests, aes(x, y1)) +  
+  labs(x = "Diameter (um)")+
+  labs(y = "STD_FD069 (pt/ml)")+
+  geom_point() +
+  labs(title = "FD069 3.5k pt/ml normalized for the area")+ theme(plot.title = element_text(hjust = 0.5))+
+  geom_smooth(method = "lm", formula = formula2) +
+  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(rr.label), sep = "~~~~")),
+               formula = formula2, rr.digits = 3, coef.digits = 2, parse = TRUE)
+
+x<-normalized_tests[,1]
+y2<-normalized_tests[,4]
+formula3 <- y2 ~ poly(x, 3, raw = TRUE)
+plot3<-ggplot(normalized_tests, aes(x, y2)) +  
+  labs(x = "Diameter (um)")+
+  labs(y = "STD_FD069 (pt/ml)")+
+  geom_point() +
+  labs(title = "FD069 7k pt/ml normalized for the area")+ theme(plot.title = element_text(hjust = 0.5))+
+  geom_smooth(method = "lm", formula = formula3) +
+  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(rr.label), sep = "~~~~")),
+               formula = formula3, rr.digits = 3, coef.digits = 2, parse = TRUE)
+
+
+library("gridExtra", lib.loc="~/R/win-library/3.5")
+grid.arrange(plot1, plot2, plot3, ncol=2)
+
+# coefficients of the cubic functions
+a <- c(-0.00019,0.012,-0.23,1.3)
+b <- c(-0.00012,0.0071,-0.13,0.71)
+c <- c(-0.00012,0.0071,-0.13,0.72)
+corr<-c(4.4*10^-5,-0.0022,-0.014,1.2)
+
+#Three arrays with corrected coefficients for each cubic function
+y1c<-a-corr
+y2c<-b-corr
+y3c<-c-corr
+#Write cubic functions corrected
+library(polynom) 
+p1 <- polynomial(c(y1c[4],y1c[3],y1c[2],y1c[1]))
+p2 <- polynomial(c(y2c[4],y2c[3],y2c[2],y2c[1]))
+p3 <- polynomial(c(y3c[4],y3c[3],y3c[2],y3c[1]))
+
+#Plot polynomial functions corrected
+par(mfrow=c(2,2)) 
+plot(p1,pch=20, col="red", 
+     ylab = "FD069_20kpt corrected",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="FD069_20kpt corrected")
+plot(p2,pch=20, col="green", 
+     ylab = "FD069_3.5kpt corrected",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="FD069_3.5kpt corrected")
+plot(p3,pch=20, col="blue", 
+     ylab = "FD069_7kpt corrected",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="FD069_7kpt corrected")
+
+#dev.off() 
 
 #ARRAY CORRECTION
 #Extraction of the first 15 diameters (3 to 45 um) from the matrix df
@@ -275,9 +385,77 @@ matrix_corrected<-data.frame(t(matrix_corrected[,1:15]))
 matrix_corrected<-data.frame(x1, matrix_corrected)
 colnames(matrix_corrected)<-c("Diameters (um)","FD069_20kpt/ml","FD069_3,5kpt/ml","FD069_7kpt/ml")
 #plot new results
-matplot(matrix_corrected[,1],matrix_corrected[,2:4],type='l', col = rainbow(3), log="xy",
+
+matplot(matrix_corrected[,1],matrix_corrected[,2:4],type='l', col = rainbow(3), 
+        main="Tests corrected",  
         xlab = expression(paste("Diameter (", mu, "m)")),
         ylab= "corrected tests (pt/ml)",
         lwd=1, lty=1)
 axis(1, at=seq(3, 47, by=3), labels = FALSE)
-legend("topright",colnames(matrix_corrected[,2:4]),col=rainbow(3),cex=0.6,fill=rainbow(5))
+legend("topright",colnames(matrix_corrected[,2:4]),col=rainbow(3),cex=0.6,fill=rainbow(3))
+
+#import milliQ.txt file
+milliQ<-read.table(file.choose(),skip=6, sep="\t", dec=",", header=TRUE,row.names=NULL) 
+milliQ<-milliQ[,3:34]
+
+flow_rate <- 2/60 #ml/s
+diameters <- c(3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96)
+
+#1st df for milliQ (600s of acquisition)
+milliQ_600s<-milliQ[1:600,]
+
+volume_milliQ_600s<-flow_rate*nrow(data.frame(milliQ_600s)) #ml
+
+milliQ_600s <- apply(data.frame(milliQ_600s),2,sum) #sum(pt)per ch. (600s)
+milliQ_600s <-data.frame(milliQ_600s)/volume_milliQ_600s #sum(pt/ml)per ch. 
+milliQ_600s<-data.frame(diameters, milliQ_600s)
+
+#2nd df for milliQ (1800s of acquisition)
+milliQ_1800s<-milliQ[1:1800,]
+
+volume_milliQ_1800s<-flow_rate*nrow(data.frame(milliQ_1800s)) #ml
+
+milliQ_1800s <- apply(data.frame(milliQ_1800s),2,sum) #sum(pt)per ch. (1800s)
+milliQ_1800s <-data.frame(milliQ_1800s)/volume_milliQ_1800s #sum(pt/ml)per ch. 
+milliQ_1800s <-data.frame(diameters, milliQ_1800s)
+
+#3rd df for milliQ (3600s of acquisition)
+milliQ_3600s<-milliQ[1:3600,]
+
+volume<-flow_rate*nrow(data.frame(milliQ_3600s)) #ml
+
+milliQ_3600s <- apply(data.frame(milliQ_3600s),2,sum) #sum(pt)per ch. (1h)
+milliQ_3600s <-data.frame(milliQ_3600s)/volume #sum(pt/ml)per ch. 
+milliQ_3600s <-data.frame(diameters, milliQ_3600s)
+
+# 3 figures arranged in 2 rows and 2 columns
+par(mfrow=c(2,2)) 
+plot(milliQ_600s$milliQ_600s~milliQ_600s$diameters,pch=20, col="goldenrod3", 
+     ylab = "milliQ pt/ml",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="milliQ pt/ml 10min")
+plot(milliQ_1800s$milliQ_1800s~milliQ_1800s$diameters,pch=20, col="orange", 
+     ylab = "milliQ pt/ml",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="milliQ pt/ml 30min")
+plot(milliQ_3600s$milliQ_3600s~milliQ_3600s$diameters,pch=20, col="green", 
+     ylab = "milliQ pt/ml",
+     xlab = expression(paste("Diameter (", mu, "m)")),
+     main="milliQ pt/ml 1h")
+
+#Create an R2 vector for milliQ tests (linear regression: raw counts vs time)
+
+time<-c(0:3603) #s
+milliQ<-data.frame(time,milliQ)
+
+R2_vec_milliQ<-sapply(milliQ[-ncol(milliQ)],function(x){
+  mylm<-lm(x~milliQ$time)
+  theR2<-summary(mylm)$r.squared
+  return(theR2)
+})
+
+R2_vec_milliQ <- na.omit(R2_vec_milliQ)
+
+
+
+
